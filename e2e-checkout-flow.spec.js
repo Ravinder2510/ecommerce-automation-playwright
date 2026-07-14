71d0dbe52b4c4debfabeed07fbdf2831d47e50d7
Ravinder2510 <saraswathiraman7@gmail.com>
2026-07-14 11:40:21 +0530

    Update e2e-checkout-flow.spec.js

--- e2e-checkout-flow.spec.js
+++ e2e-checkout-flow.spec.js
@@ -10,7 +10,7 @@ test('E-Commerce End to End Checkout Flow', async ({ page }) => {
   await page.locator('[data-test="login-button"]').click();
 
   // 3. Verify user landed on products page
-  await expect(page).toHaveURL('https://saucedemo.cominventory.html');
+  await expect(page).toHaveURL('https://saucedemo.com');
 
   // 4. Add an item to the cart
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
